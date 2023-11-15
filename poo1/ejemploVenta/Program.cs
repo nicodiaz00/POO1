f5a3e82795cb0893b865c778004b60489a7752f2
Nico <ngdiaz00@gmail.com>
2023-11-15 10:22:59 -0300

    terminando ejemplo ventas

--- poo1/ejemploVenta/Program.cs
+++ poo1/ejemploVenta/Program.cs
@@ -23,6 +23,24 @@ namespace ejemploVenta
                 Console.WriteLine("Ingrese cod marca, mayor a 0 y menor a 11");
                 producto[i].setCodMarca(int.Parse(Console.ReadLine()));
             }
+
+            Ventas ventaSemanal = new Ventas();
+            int codigoCliente;
+            int codigoArticulo;
+            int cantidad;
+            Console.WriteLine("ingrese codigo Cliente");
+            codigoCliente = int.Parse(Console.ReadLine());
+            ventaSemanal.CodigoCliente = codigoCliente;
+            while(ventaSemanal.CodigoCliente != 0)
+            {
+                Console.WriteLine("Ingrese codigo articulo");
+                codigoArticulo = int.Parse(Console.ReadLine());
+                ventaSemanal.CodigoArticulo = codigoArticulo;
+                Console.WriteLine("Ingrese la cantidad");
+                cantidad = int.Parse(Console.ReadLine());
+                ventaSemanal.Cantidad = cantidad;
+            }
+
         }
     }
 }
---
+++ poo1/ejemploVenta/Ventas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemploVenta
+{
+    internal class Ventas
+    {
+        public int CodigoCliente { get; set; }
+        public int Cantidad { get; set; }
+        public int CodigoArticulo { get; set; }
+
+    }
+}
