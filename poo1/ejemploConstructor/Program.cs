f83e59763c1999579ade6b8e56c3742b3280b6f3
Nico <ngdiaz00@gmail.com>
2023-11-15 10:57:52 -0300

    cambiando propiedades de objeto

--- poo1/ejemploConstructor/Program.cs
+++ poo1/ejemploConstructor/Program.cs
@@ -10,6 +10,26 @@ namespace ejemploConstructor
     {
         static void Main(string[] args)
         {
+            Botella botella1 = new Botella("Rojo", "Plastico");
+            string cambioColor = "";
+            string cambioMaterial = "";
+
+            Console.WriteLine("el color es " +  botella1.getColor());
+            Console.WriteLine("Ingrese el nuevo color");
+            cambioColor = Console.ReadLine();
+
+            botella1.setColor(cambioColor);
+
+            Console.WriteLine("ingrese nuevo material");
+            cambioMaterial = Console.ReadLine();
+            botella1.setMaterial(cambioMaterial);
+
+            Console.WriteLine("Ahora el color es " + botella1.getColor());
+            Console.WriteLine("Ahora el material es " + botella1.getMaterial());
+
+            Console.ReadKey();
         }
+
+        
     }
 }
