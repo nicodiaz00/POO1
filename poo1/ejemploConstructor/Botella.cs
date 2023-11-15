189d82963c170e529ebb17bef3dac581385343c6
Nico <ngdiaz00@gmail.com>
2023-11-15 10:47:26 -0300

    creando constructores

---
+++ poo1/ejemploConstructor/Botella.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemploConstructor
+{
+    internal class Botella
+    {
+        public Botella(string color, string material)
+        {
+            this.color = color;
+            this.material = material;
+        }
+        private string color;
+        private string material;
+        private int capacidad;
+
+
+        public void setCapacidad( int cap)
+        {
+            capacidad = cap;
+        }
+        public int getCapacidad()
+        {
+            return capacidad;
+        }
+
+        public void setColor(string valorColor)
+        {
+            color = valorColor;
+        }
+        public string getColor()
+        {
+            return color;
+        }
+        public void setMaterial(string valorMaterial)
+        {
+            material = valorMaterial;
+        }
+        public string getMaterial()
+        {
+            return material;
+        }
+    }
+}
