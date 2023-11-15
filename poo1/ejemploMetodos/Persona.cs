ce437185d55280c81f295073bc4f0d46e422531d
Nico <ngdiaz00@gmail.com>
2023-11-15 11:40:43 -0300

    creando metodos a mi objeto persona

---
+++ poo1/ejemploMetodos/Persona.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemploMetodos
+{
+    internal class Persona
+    {
+        private string nombre;
+        private int edad;
+        private string apellido;
+
+        public void setNombre(string name)
+        {
+            nombre = name;
+        }
+        public string getNombre()
+        {
+            return nombre;
+        }
+        public void setEdad(int age)
+        {
+            edad = age;
+        }
+        public int getEdad()
+        {
+            return edad;
+        }
+        public void setApellido(string lastName)
+        {
+            apellido = lastName;
+        }
+        public string getApellido()
+        {
+            return apellido;
+        }
+
+        //METODO
+
+        public string saludar()
+        {
+            return "hola soy " + nombre;
+        }
+    }
+}
---
+++ poo1/ejemploMetodos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemploMetodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona personaNueva = new Persona();
+
+            string nombre;
+
+            Console.WriteLine("ingrese el nombre de la persona");
+            nombre = Console.ReadLine();
+
+            personaNueva.setNombre(nombre);
+
+            Console.WriteLine("El nombre de la persona es" + personaNueva.getNombre());
+
+            Console.WriteLine(personaNueva.saludar());
+
+            Console.ReadKey();
+        }
+    }
+}
