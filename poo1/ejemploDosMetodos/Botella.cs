5218f4d0d273b3f797816ca3a32c04bd8519a699
Nico <ngdiaz00@gmail.com>
2023-11-15 12:54:00 -0300

    agrego metodos y funciones

---
+++ poo1/ejemploDosMetodos/Botella.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemploDosMetodos
+{
+    internal class Botella
+    {
+        public Botella(string color, string material)
+        {
+            this.color = color;
+            this.material = material;
+            capacidad = 100;
+            cantidadActual = 0;
+
+
+        }
+        private string color;
+        private string material;
+        private float capacidad;
+        private float cantidadActual;
+
+        public string Color
+        {
+            get { return color; }
+        }
+        public string Material
+        {
+            get { return material; }
+        }
+
+        public float Capacidad
+        {
+            get { return capacidad; }
+            set { capacidad = value; }
+        }
+
+        public float CantidadActual
+        {
+            get { return cantidadActual; }
+            set { cantidadActual = value; }
+        }
+
+        public float recargar()
+        {
+            int costo = 50;
+            if(cantidadActual == 0)
+            {
+                cantidadActual = 100;
+                return costo;
+            }
+            else
+            {
+                float costoFinal = calculcarCosto(cantidadActual, costo);
+                return costoFinal;
+            }
+            
+
+        }
+
+        float calculcarCosto(float valorcantidad, int valorDiferencia)
+        {
+            float costoTotal = (valorcantidad * valorDiferencia) / 100;
+            return costoTotal;
+        }
+    }
+}
---
+++ poo1/ejemploDosMetodos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejemploDosMetodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Botella botellaNueva = new Botella("rojo", "aluminio");
+            float cantidadMomentanea;
+
+            Console.WriteLine("la capacidad maxima de la botella es " + botellaNueva.Capacidad);
+            Console.WriteLine("la cantidad actual es " + botellaNueva.CantidadActual);
+
+            Console.WriteLine("ingrese cuanto quiere ponerle a su botella");
+            cantidadMomentanea = float.Parse(Console.ReadLine());
+            botellaNueva.CantidadActual = cantidadMomentanea;
+
+            Console.WriteLine("la cantidad ahora es " + botellaNueva.CantidadActual);
+            Console.WriteLine("Ahora debe pagar " + botellaNueva.recargar());
+
+            Console.ReadKey();
+        }
+    }
+}
